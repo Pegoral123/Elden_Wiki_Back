aa074931dfbf80f87fcc9c54d956fee17443f613
Pegoral123 <jhonathanpegoral123@gmail.com>
2025-10-06 22:27:17 -0300

    rota de bosses definidas

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
 
@@ -28,6 +28,36 @@ def get_bosses():
     else:
         return {"erro": "ao buscar os dados dos bosses"}
     
+@app.get("/bosses/{boss_name}")
+def get_boss_by_name(boss_name: str):
+    bosses = [
+        {
+            "id": 1,
+            "name": "Margit, o Caído",
+            "location": "Castelo Stormveil, Limgrave",
+            "description": "Margit é um chefe opcional que serve como guardião da entrada do Castelo Stormveil. Ele é um inimigo formidável que testa as habilidades dos jogadores logo no início do jogo.",
+        },
+        {
+            "id": 2,
+            "name": "Godrick, o Enxertado",
+            "location": "Castelo Stormveil, Limgrave",
+            "description": "Godrick é o primeiro grande chefe do jogo e um dos portadores de Grandes Runas. Ele é um descendente degenerado da linhagem dourada de Marika, obcecado por poder a ponto de enxertar membros de outros guerreiros em si mesmo.",
+        },
+        {
+            "id": 3,
+            "name": "Radahn, o Flagelo Estelar",
+            "location": "Caelid",
+            "description": "Radahn é um dos mais poderosos e temidos chefes de Elden Ring, conhecido por sua habilidade em combate e por ser um dos portadores de Grandes Runas.",
+        },
+    ]
+
+    for boss in bosses:
+        if boss["name"].lower() == boss_name.lower() or str(boss["id"]) == boss_name:
+            return boss
+
+    # Retorna um erro 404 se o boss não for encontrado
+    raise HTTPException(status_code=404, detail="Boss não encontrado")
+
 @app.get("/limgrave")
 
 def get_limgrave():
