5172c1347808c1c3a9db14d1d8be81a27d6501d7
Pegoral123 <jhonathanpegoral123@gmail.com>
2025-11-08 11:53:56 -0300

    refatorar: definir origens permitidas para CORS em uma variável

--- main.py
+++ main.py
@@ -9,16 +9,24 @@ from routes import auth, bosses, locations
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
+
 app.include_router(locations.router)
 app.include_router(bosses.router)
 app.include_router(auth.router)
 
+origins = [
+    "http://localhost:5173",  # A origem do seu app Vue
+    "http://127.0.0.1:5173", 
+    "http://localhost:8080",# Adicione variações se necessário
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Ou especifique ["http://localhost:8080"]
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*", "Authorization"],
 )
 
 @app.get("/")
